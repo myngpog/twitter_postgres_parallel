f2c2c48a142108fb9174784432cc36aaff45faca
My <pingpengpongpog@gmail.com>
2024-04-05 20:59:16 -0700

    old stuff check

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -6,16 +6,17 @@ echo '==========================================================================
 echo 'load denormalized'
 echo '================================================================================'
 time for file in $files; do
-    echo
-    # copy your solution to the twitter_postgres assignment here
+    # call the load_tweets.py file to load data into pg_normalized
+    python3 load_tweets.py --db=postgresql://postgres:pass@localhost:13549/postgres --inputs "$file"
 done
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
 time for file in $files; do
-    echo
-    # copy your solution to the twitter_postgres assignment here
+    # use SQL's COPY command to load data into pg_denormalized
+    zcat $files | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:13649/postgres -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+
 done
 
 echo '================================================================================'

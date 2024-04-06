1301c7cc27bdc4304fb50018d6a028d10178632a
My <pingpengpongpog@gmail.com>
2024-04-05 21:10:09 -0700

    fixed ports

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -6,6 +6,7 @@ echo '==========================================================================
 echo 'load denormalized'
 echo '================================================================================'
 time for file in $files; do
+    echo
     # call the load_tweets.py file to load data into pg_normalized
     python3 load_tweets.py --db=postgresql://postgres:pass@localhost:13549/postgres --inputs "$file"
 done
@@ -14,6 +15,7 @@ echo '==========================================================================
 echo 'load pg_normalized'
 echo '================================================================================'
 time for file in $files; do
+    echo
     # use SQL's COPY command to load data into pg_denormalized
     zcat $files | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:13649/postgres -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 
@@ -23,5 +25,5 @@ echo '==========================================================================
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 time for file in $files; do
-    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3/ --inputs $file
+    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:13944/ --inputs $file
 done
